9b0d9e550cc4120e493159f9a52134b5368ab602
Emilio Ferrera <92113796+efermon@users.noreply.github.com>
2023-06-08 14:24:41 +0200

    Add files via upload

---
+++ leedigito.js
@@ -0,0 +1,91 @@
+const COLOR = "black";
+const COLOR_FONDO = "white"
+const GROSOR = 6;
+
+let x0 = 0, y0 = 0, x1 = 0, y1 = 0;
+let swDibujo = false; 
+let ctx = null;
+
+
+function inicio(){
+    const canva = document.getElementById("canva");
+    ctx = canva.getContext("2d");
+
+    canva.width = 215;
+    canva.height = 215;
+
+
+    canva.addEventListener("mousedown", mouse_down, false)
+    canva.addEventListener("mousemove", mouse_move, false)
+    canva.addEventListener("mouseup", mouse_up, false)
+    canva.addEventListener("mouseout", mouse_out, false)
+
+
+
+    const btnbor = document.getElementById("btnBor");
+    btnbor.addEventListener("click", Limpia_canvas, false);
+
+    const btnver = document.getElementById("btnVer");
+    btnver.addEventListener("click", Reconocer, false);
+
+    
+    Limpia_canvas()
+
+}   
+
+function situaX(X) {
+    return X - canva.getBoundingClientRect().left;
+}
+function situaY(Y) {
+    return Y - canva.getBoundingClientRect().top;
+}
+
+function Limpia_canvas() {
+    ctx.fillStyle = COLOR_FONDO;
+    ctx.fillRect(0, 0, canva.width, canva.height);
+}
+
+function mouse_down(evento) {
+    x0 = x1;
+    y0 = y1;
+    x1 = situaX(evento.clientX);
+    y1 = situaY(evento.clientY);
+    ctx.beginPath();
+    ctx.fillStyle = COLOR_FONDO;
+    ctx.fillRect(x1, y1, GROSOR, GROSOR);
+    ctx.closePath();
+    swDibujo = true;
+}
+
+function mouse_move(evento) {
+    if (!swDibujo) {
+        return;
+    }
+
+    x0 = x1;
+    y0 = y1;
+    x1 = situaX(evento.clientX);
+    y1 = situaY(evento.clientY);
+    ctx.beginPath();
+    ctx.moveTo(x0, y0);
+    ctx.lineTo(x1, y1);
+    ctx.strokeStyle = COLOR;
+    ctx.lineWidth = GROSOR;
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function mouse_up() {
+    swDibujo = false;
+}
+function mouse_out() {
+    swDibujo = false;
+}
+
+function Reconocer() {
+    const digito = document.getElementById("imagen");
+	digito.src = canva.toDataURL("image/png");	
+}
+
+window.addEventListener("load", inicio, false);
+
